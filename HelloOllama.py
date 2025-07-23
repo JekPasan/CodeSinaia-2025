160a9afadbd38dfea3477e2bda4ad717bcd86fe7
JekPasan <matorurazvan@gmail.com>
2025-07-23 09:38:04 +0300

    asdk

--- HelloOllama.py
+++ HelloOllama.py
@@ -4,7 +4,7 @@ import ollama
 model_name = "gemma3:1b"
 
 # Define the prompt
-prompt = "What is the capital of France?"
+prompt = "Hello World"
 
 # Send the prompt to the model and get the response
 response = ollama.generate(model=model_name, prompt=prompt)
